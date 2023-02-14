15e9638483c8aa1e8d8ce87f832b685fd31e32a2
jdluis <jdluisdev@gmail.com>
2023-02-14 15:47:00 +0000

    iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,24 @@
-const router = require("express").Router()
+const router = require("express").Router();
+
+const Celebrity = require("../models/Celebrity.model.js");
+
+// GET "celebrities/create"
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+// POST "celebrities/create"
+router.post("/create", async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.create({
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+    });
+    res.redirect("/celebrities");
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
