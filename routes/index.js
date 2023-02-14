2df1ab481183da4230c5fc9856cea46fe5884bee
jdluis <jdluisdev@gmail.com>
2023-02-14 15:01:44 +0000

    Iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
+const celebrities = require ("./celebrities.routes");
+const movies = require ("./movies.routes");
+
+router.use("/celebrities", celebrities);
+router.use("/movies", movies);
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
