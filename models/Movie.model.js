83fb605078796f1abd5d5b079de4de63caeb242e
jdluis <jdluisdev@gmail.com>
2023-02-14 16:25:55 +0000

    Iteration 5

---
+++ models/Movie.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    genre: String,
+    plot: String,
+    cast: {
+        type: mongoose.Types.ObjectId,
+        ref: "Celebrity"
+    }
+})
+
+const Movie = mongoose.Model("Movie", movieSchema);
+module.exports = Movie;
