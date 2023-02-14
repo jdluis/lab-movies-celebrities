6c0d0668fc20bc875ab47b900e3b4fa3e969e034
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-02-14 23:37:59 +0100

    intento de css

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -35,6 +35,60 @@ router.post("/create", async (req, res, next) => {
   }
 });
 
+// GET "celebrities/:id/celebrity-details" => Detalles de celebridad
+router.get("/:id/celebrity-details", async (req, res, next) => {
+    try{
+      const { id } = req.params
+      const celebrity = await Celebrity.findById(id)
+      res.render(`celebrities/celebrity-details.hbs`,{
+        celebrity: celebrity
+      })
+    } catch(err){
+      next(err)
+    }
+})
+
+// GET "celebrities/:id/edit-celebrity" => editar celebridad
+
+router.get("/:id/edit-celebrity", async (req, res, next) => {
+  try{
+    const { id } = req.params
+    const celebrity = await Celebrity.findById(id)
+    res.render("celebrities/edit-celebrity.hbs", {
+      celebrity: celebrity
+    })
+  } catch(err){
+    next(err)
+  }
+  
+})
+
+// POST "celebrities/:id/edit-celebrity" => Recibiendo la informacion de update celebridad
+router.post("/:id/edit-celebrity", async (req, res, next) => {
+    try{
+      const { id } = req.params
+      const celebrity = await Celebrity.findByIdAndUpdate(id, {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+      })
+      res.redirect("/celebrities")
+
+    } catch(err){
+      next(err)
+    }
+})
+
+// POST "celebrities/:id" => DELETE celebridad
+router.post("/:id", async (req, res, next) => {
+  try{
+    const { id } = req.params
+    await Celebrity.findByIdAndDelete(id)
+    res.redirect("/celebrities")
+  } catch(err){
+    next(err)
+  }
+})
 
 
 
